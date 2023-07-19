0a1bb9ac61378aa3d7d4c51058c7946f46c9a900
Pastafredda <justspotify09@gmail.com>
2023-07-19 20:05:43 +0200

    validation

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -23,7 +23,15 @@ class MainController extends Controller
     }
 
     public function store(Request $request){
-        $data = $request -> all();
+        $data = $request -> validate([
+            'title' => 'required|min:4|max:128',
+            'description' => 'required|min:4|max:400',
+            'thumb' => 'required|min:200',
+            'price' => 'required|integer|numeric',
+            'series' => 'required|min:4|max:128',
+            'sale_date' => 'required|date',
+            'type' => 'required|min:4|max:64'
+        ]);
         $comic = Comic :: create($data);
 
         return redirect()-> route("comic.index");
